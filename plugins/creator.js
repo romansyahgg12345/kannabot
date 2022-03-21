448cc0bbf1d2753c9b21e925b945c60d4301c4cd
Yuda Official <67179682+romansyahgg12345@users.noreply.github.com>
2022-03-21 17:21:02 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,12 +1,12 @@
 /*const { MessageType } = require('@adiwajshing/baileys')
 const PhoneNumber = require('awesome-phonenumber')
 async function handler(m) {
-  let name = 'ᴏωɴᴇŗ ķαηnαɓσт あ'
+  let name = 'ᴏωɴᴇŗ YudaBotz あ'
   number = owner[0].replace(/[^0-9]/g, '')
   let njid = number + '@s.whatsapp.net'
   let onW = await this.isOnWhatsApp(njid) || { isBusiness: false }
 
-  let name2 = '๑ོƘαŋnα ʙσţ๑'
+  let name2 = '๑ོYyudaBotz ʙσţ๑'
   number2 = owner[1].replace(/[^0-9]/g, '')
   let njid2 = number2 + '@s.whatsapp.net'
   let onW2 = await this.isOnWhatsApp(njid2) || { isBusiness: false }
